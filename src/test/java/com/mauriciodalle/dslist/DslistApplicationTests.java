e5d2c6d8e110cfa6173e5fc331c1becab15380a6
Mauricio <mauriciodalle01@gmail.com>
2025-08-22 20:19:00 -0300

    Project created

---
+++ src/test/java/com/mauriciodalle/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauriciodalle.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
